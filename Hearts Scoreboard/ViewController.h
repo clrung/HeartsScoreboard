3ec7347f55e30a350dc68b18a9036bdecc678b18
Christopher Rung <clrung@gmail.com>
2015-02-26 22:03:51 -0500

    Initial Commit

---
+++ Hearts Scoreboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hearts Scoreboard
+//
+//  Created by Christopher Rung on 2/26/15.
+//  Copyright (c) 2015 Christopher Rung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Hearts ScoreboardTests/Hearts_ScoreboardTests.m
@@ -0,0 +1,40 @@
+//
+//  Hearts_ScoreboardTests.m
+//  Hearts ScoreboardTests
+//
+//  Created by Christopher Rung on 2/26/15.
+//  Copyright (c) 2015 Christopher Rung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Hearts_ScoreboardTests : XCTestCase
+
+@end
+
+@implementation Hearts_ScoreboardTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
