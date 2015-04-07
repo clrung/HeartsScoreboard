a2bbd785771c54059318b565574ee0ed6de49394
Christopher Rung <clrung@gmail.com>
2015-04-07 11:59:30 -0400

    Added custom CollectionViewCell, which stores a UILabel to display the round score.

---
+++ Hearts Scoreboard/ScoreCollectionViewCell.h
@@ -0,0 +1,14 @@
+//
+//  ScoreCollectionViewCell.h
+//  Hearts Scoreboard
+//
+//  Created by Christopher Rung on 4/6/15.
+//  Copyright (c) 2015 Christopher Rung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScoreCollectionViewCell : UICollectionViewCell
+@property (strong, nonatomic) IBOutlet UILabel *scoreLabel;
+
+@end
---
+++ Hearts Scoreboard/ScoreCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  ScoreCollectionViewCell.m
+//  Hearts Scoreboard
+//
+//  Created by Christopher Rung on 4/6/15.
+//  Copyright (c) 2015 Christopher Rung. All rights reserved.
+//
+
+#import "ScoreCollectionViewCell.h"
+
+@implementation ScoreCollectionViewCell
+
+@end
