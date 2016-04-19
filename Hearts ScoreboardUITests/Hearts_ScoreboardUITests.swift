5a3d915baf79a198895630586f3762dad3a47e9e
Christopher Rung <clrung@gmail.com>
2016-04-19 12:45:19 -0400

    finalizing stuff for v.2.1 (screenshots, etc)

--- Hearts ScoreboardUITests/Hearts_ScoreboardUITests.swift
+++ Hearts ScoreboardUITests/Hearts_ScoreboardUITests.swift
@@ -17,37 +17,39 @@ class Hearts_ScoreboardUITests: XCTestCase {
         setupSnapshot(app)
         app.launch()
     }
-    
+
     func testExample() {
         let app = XCUIApplication()
         app.buttons["Settings Button"].tap()
         
         let elementsQuery = app.scrollViews.otherElements
-        elementsQuery.textFields["Player 1"].tap()
+        let player1TextField = elementsQuery.textFields["Player 1"]
+        player1TextField.tap()
         
-        let elementsQuery2 = elementsQuery
-        elementsQuery2.buttons["Clear text"].tap()
+        elementsQuery.buttons["Clear text"].tap()
         elementsQuery.textFields["Player 1"].typeText("Christopher");
-        
-        let toolbarsQuery = app.toolbars
-        let forwardButton = toolbarsQuery.buttons["Forward"]
+
+        let forwardButton = app.toolbars.buttons["Forward"]
         forwardButton.tap()
-        elementsQuery2.buttons["Clear text"].tap()
+        
+        elementsQuery.buttons["Clear text"].tap()
         elementsQuery.textFields["Player 2"].typeText("Mary");
         forwardButton.tap()
-        elementsQuery2.buttons["Clear text"].tap()
+        elementsQuery.buttons["Clear text"].tap()
         elementsQuery.textFields["Player 3"].typeText("George");
         forwardButton.tap()
-        elementsQuery2.buttons["Clear text"].tap()
+        elementsQuery.buttons["Clear text"].tap()
         elementsQuery.textFields["Player 4"].typeText("Cathy");
-        toolbarsQuery.buttons["Done"].tap()
+
+        app.toolbars.buttons["Done"].tap()
         
         snapshot("2Settings")
         
-        app.buttons["Settings Button"].tap()
+        let settingsButtonButton = app.buttons["Settings Button"]
+        settingsButtonButton.tap()
         app.buttons["Start Game"].tap()
         
-        let element = app.otherElements.containingType(.Button, identifier:"Settings Button").childrenMatchingType(.Other).elementBoundByIndex(2)
+        let element = app.otherElements.containingType(.Button, identifier:"Settings Button").childrenMatchingType(.Other).elementBoundByIndex(3)
         let qButton = element.childrenMatchingType(.Button).matchingIdentifier("Q♠︎").elementBoundByIndex(0)
         qButton.tap()
         
@@ -56,8 +58,7 @@ class Hearts_ScoreboardUITests: XCTestCase {
         button.tap()
         element.childrenMatchingType(.Button).matchingIdentifier("+     ").elementBoundByIndex(2).tap()
         
-        let button2 = element.childrenMatchingType(.Button).matchingIdentifier("+     ").elementBoundByIndex(3)
-        button2.tap()
+        let button2 = element.childrenMatchingType(.Button).matchingIdentifier("+2").elementBoundByIndex(3)
         button2.tap()
         
         let submitButton = app.buttons["Submit"]
@@ -86,7 +87,6 @@ class Hearts_ScoreboardUITests: XCTestCase {
         let button5 = element.childrenMatchingType(.Button).matchingIdentifier("+5").elementBoundByIndex(2)
         button5.tap()
         button2.tap()
-        button2.tap()
         submitButton.tap()
         nextRoundButton.tap()
         element.childrenMatchingType(.Button).matchingIdentifier("Moon").elementBoundByIndex(2).tap()
@@ -100,14 +100,15 @@ class Hearts_ScoreboardUITests: XCTestCase {
         
         snapshot("1MainScreen")
         
-        app.buttons["Settings Button"].tap()
+        settingsButtonButton.tap()
         app.buttons["Light"].tap()
-        app.buttons["Settings Button"].tap()
+        settingsButtonButton.tap()
         snapshot("4Light");
         
-        app.buttons["Settings Button"].tap()
+        
+        settingsButtonButton.tap()
         app.buttons["Dark"].tap()
-        app.buttons["Settings Button"].tap()
+        settingsButtonButton.tap()
         snapshot("5Dark");
         
     }
